fde91a87188a579e211b269970a4ba84d0000529
Danilo <efim.dan04@gmail.com>
2023-11-30 19:57:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("\nProgramming***Essentials***in...Python")
+(print
+("""
+    *
+   * *
+  *   *
+ *     *
+***   ***
+  *   *
+  *   *
+  *****
+"""))
+print("I'm student\n")
+print('"Im"\n"learning"\n"""Python\n"""')
+print(500**8)
+print("\n")
+print(777**16)
